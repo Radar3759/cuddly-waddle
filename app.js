9f46222ea89bf07f8e269887c566d80df9ffd480
Radar3759 <bunnybridgemitten@gmail.com>
2021-08-09 10:54:32 -0700

    "add rectangle.js"

--- app.js
+++ app.js
@@ -1,15 +1,14 @@
-//make a rectangle define maths for perimeter and area
- const rect = {
-     perimeter: (x,y) => 2 * (x+y),
-     area: (x,y) => x * y
- };
+//calls rectangle.js instead of storing Maths here
+ const rect = require('./rectangle');
+ 
+ //define maths for perimeter and area
 // perimeter: (x, y) => 2 * (x + y),
 // area: (x, y) => x * y
 
 // //function to solve rect using params l, w
 function solveRect(l, w) {
      //log initial params
-     console.log(`Solving for rectangle wiwth dimensions: ${l}, ${w}`);
+     console.log(`Solving for rectangle width dimensions: ${l}, ${w}`);
      //if both l and w are less than or equal to zero log this message in backticks
      if (l <= 0 || w <= 0) {
          console.log(`Rectangle dimensions must be greater than zero. Received: ${l}, ${w}`);
@@ -19,24 +18,7 @@ function solveRect(l, w) {
          console.log(`Perimeter of rectangle: ${rect.perimeter(l, w)}`);
      }
  }
-
-// const rect = {
-//     perimeter: (x, y) => 2 * (x + y),
-//     area: (x, y) => x * y
-// };
-
-// function solveRect(l, w) {
-//     console.log(`Solving for rectangle with dimensions: ${l}, ${w}`);
-
-//     if (l <= 0 || w <= 0) {
-//         console.log(`Rectangle dimensions must be greater than zero. Received: ${l}, ${w}`);
-//     } else {
-//         console.log(`Area of rectangle: ${rect.area(l, w)}`);
-//         console.log(`Perimeter of rectangle: ${rect.perimeter(l, w)}`);
-//     }
-// }
-
-
+//test the function solveRect with different params
 solveRect(2,4);
 solveRect(3,5);
 solveRect(0, 4);
---
+++ rectangle.js
@@ -0,0 +1,4 @@
+//can also be module.exports
+//creates a file with two exports
+exports.perimeter = ( x, y ) => 2 * ( x + y);
+exports. area = (x, y ) => x * y;

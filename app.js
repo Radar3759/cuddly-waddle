607b75706a898cc1366450cb6a98d61041b723ad
Radar3759 <bunnybridgemitten@gmail.com>
2021-08-09 11:36:56 -0700

    "Node Callbacks and Error Handling"

--- app.js
+++ app.js
@@ -1,23 +1,22 @@
-//calls rectangle.js instead of storing Maths here
+//change declaration to rectangle.js instead of storing Maths here
  const rect = require('./rectangle');
- 
- //define maths for perimeter and area
-// perimeter: (x, y) => 2 * (x + y),
-// area: (x, y) => x * y
 
-// //function to solve rect using params l, w
 function solveRect(l, w) {
-     //log initial params
-     console.log(`Solving for rectangle width dimensions: ${l}, ${w}`);
-     //if both l and w are less than or equal to zero log this message in backticks
-     if (l <= 0 || w <= 0) {
-         console.log(`Rectangle dimensions must be greater than zero. Received: ${l}, ${w}`);
-     } else {
+    console.log(`Solving for rectangle width dimensions: ${l}, ${w}`);
+     
+    rect(l, w, (err, rectangle) => { 
+    //if the callback is an error do this    
+        if(err) {
+            console.log('ERROR:', err.message);
+    //if the callback is valid do this 
+    } else {
         //otherwise log these messages and results
-         console.log(`Area of rectangle: ${rect.area(l,w)}`);
-         console.log(`Perimeter of rectangle: ${rect.perimeter(l, w)}`);
+         console.log(`Area of rectangle with dimensions ${l}, ${w} is: ${rectangle.area()}`);
+         console.log(`Perimeter of rectangle with dimensions ${l}, ${w} is: ${rectangle.perimeter()}`);
      }
- }
+    });
+    console.log(`This statement is logged after the call to rect()`);
+}    
 //test the function solveRect with different params
 solveRect(2,4);
 solveRect(3,5);
--- rectangle.js
+++ rectangle.js
@@ -1,4 +1,21 @@
-//can also be module.exports
-//creates a file with two exports
-exports.perimeter = ( x, y ) => 2 * ( x + y);
-exports. area = (x, y ) => x * y;
+//assign a function as a value for the entire export
+module.exports = (x, y, callback) => {
+    //if x or y is less <= 0
+    if (x <= 0 || y <= 0 ) {
+        callback(new Error(`Rectangle dimensions must be greater than zero. Received: ${x}, ${y}`));
+    } else {
+        //use ms
+        setTimeout(() =>
+            callback(null, {
+                //because x, y are in the global function, they don't need to be here
+                perimeter: () => 2 * ( x + y),
+                area: () => x * y
+            }),
+            //how many ms to wait
+            2000
+        );
+    }
+};
+
+
+
